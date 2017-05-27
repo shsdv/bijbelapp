63a0822415c1715ca7b92f5b51727de9382288f9
Geluk <sjg50@users.noreply.github.com>
2017-05-27 17:23:22 +0200

    Scroll bar bij menus

--- BijbelApp/src/pages/menu-page/menu-page.ts
+++ BijbelApp/src/pages/menu-page/menu-page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MenuItems } from '../../providers/menu-items';
 import { ContentPage } from '../content-page/content-page';
+import { Scroll } from 'ionic-angular';
+
 /**
  * Generated class for the MenuPage page.
  *
@@ -14,30 +16,48 @@ import { ContentPage } from '../content-page/content-page';
   templateUrl: 'menu-page.html',
 })
 export class MenuPage {
+  @ViewChild(Scroll) content: Scroll;
   pageTitle: any;
   items: any;
   menuService: MenuItems;
+  level : any;
+  scroller : string = "100";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.level = 1;
+    
     this.pageTitle = navParams.get("title");
     this.menuService = new MenuItems(navParams.get("dbname"));
-
+    if(navParams.get("newLevel")){
+      this.level = navParams.get("newLevel");
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
     this.menuService.getMenuItems().then((data) => {
       this.items = data;
-      console.log(data);
     });
   }
 
-  openPage(item){
+  openPage(item) {
     console.log(item);
-    if(item.hasOwnProperty("dbname")){
-      this.navCtrl.push(MenuPage, item);
-    } else if(item.hasOwnProperty("contentDB")){
+    if (item.hasOwnProperty("dbname")) {
+      item.newLevel = this.level + 1;
+        this.navCtrl.push(MenuPage, item);
+    } else if (item.hasOwnProperty("contentDB")) {
       this.navCtrl.push(ContentPage, item);
     }
   }
- 
+
+  showVal(value) {
+    let selectingId = Math.round((this.items.length - 1) * (100 - value) / 100);
+    let element = document.getElementById("id-" +this.level + '-' + selectingId);
+    console.log(element);
+    console.log("id-" + selectingId);
+    if (element) {
+      console.log("Scrolling");
+      element.scrollIntoView();
+    }
+
+  }
 }
