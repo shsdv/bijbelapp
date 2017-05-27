afccde860f49526a2d10ccc289798e1fb76fab7b
Geluk <sjg50@users.noreply.github.com>
2017-05-27 20:07:28 +0200

    Scrolling beter gemaakt

--- BijbelApp/src/pages/menu-page/menu-page.ts
+++ BijbelApp/src/pages/menu-page/menu-page.ts
@@ -16,22 +16,39 @@ import { Scroll } from 'ionic-angular';
   templateUrl: 'menu-page.html',
 })
 export class MenuPage {
-  @ViewChild(Scroll) content: Scroll;
+  @ViewChild(Scroll) ListScroller: Scroll;
   pageTitle: any;
   items: any;
   menuService: MenuItems;
   level : any;
-  scroller : string = "100";
+  scroller : string;
+  automaticChange: boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.level = 1;
-    
+    this.scroller = "100";
     this.pageTitle = navParams.get("title");
     this.menuService = new MenuItems(navParams.get("dbname"));
     if(navParams.get("newLevel")){
       this.level = navParams.get("newLevel");
     }
   }
+  ngAfterViewInit() {
+    console.log(this.ListScroller);
+        this.ListScroller.addScrollEventListener(this.listScrolled.bind(this));
+    }
+listScrolled(event){
+  if(!this.automaticChange){
+    console.log("Listview scrolled");
+    let middle = 100- Math.round((event.target.scrollTop + event.target.offsetHeight*0.5)/event.target.scrollHeight*100);
+    this.scroller = "" + middle;
 
+  }
+  console.log("Listview scrolling");
+  this.automaticChange = false;
+
+
+ // element.value = ""+ middle;
+}
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
     this.menuService.getMenuItems().then((data) => {
@@ -49,14 +66,17 @@ export class MenuPage {
     }
   }
 
-  showVal(value) {
-    let selectingId = Math.round((this.items.length - 1) * (100 - value) / 100);
+  showVal() {
+    console.log("Scroller: " + this.scroller);
+    let selectingId = Math.round((this.items.length - 1) * (100 - Number(this.scroller)) / 100);
     let element = document.getElementById("id-" +this.level + '-' + selectingId);
     console.log(element);
     console.log("id-" + selectingId);
     if (element) {
       console.log("Scrolling");
+      this.automaticChange = true;
       element.scrollIntoView();
+
     }
 
   }
