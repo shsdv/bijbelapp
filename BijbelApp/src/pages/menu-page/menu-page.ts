f26ceb6a4e053286aa791912022d6b2d23da2f1c
Geluk <sjg50@users.noreply.github.com>
2017-05-28 15:40:04 +0200

    Verbeteren van scroll ervaring, scrollbalken lopen nu netjes gelijk

--- BijbelApp/src/pages/menu-page/menu-page.ts
+++ BijbelApp/src/pages/menu-page/menu-page.ts
@@ -20,35 +20,37 @@ export class MenuPage {
   pageTitle: any;
   items: any;
   menuService: MenuItems;
-  level : any;
-  scroller : string;
+  level: any;
+  scroller: string;
   automaticChange: boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.level = 1;
     this.scroller = "100";
     this.pageTitle = navParams.get("title");
     this.menuService = new MenuItems(navParams.get("dbname"));
-    if(navParams.get("newLevel")){
+    if (navParams.get("newLevel")) {
       this.level = navParams.get("newLevel");
     }
   }
   ngAfterViewInit() {
     console.log(this.ListScroller);
-        this.ListScroller.addScrollEventListener(this.listScrolled.bind(this));
-    }
-listScrolled(event){
-  if(!this.automaticChange){
-    console.log("Listview scrolled");
-    let middle = 100- Math.round((event.target.scrollTop + event.target.offsetHeight*0.5)/event.target.scrollHeight*100);
-    this.scroller = "" + middle;
-
+    this.ListScroller.addScrollEventListener(this.listScrolled.bind(this));
   }
-  console.log("Listview scrolling");
-  this.automaticChange = false;
+  listScrolled(event) {
+    if (!this.automaticChange) {
+      console.log("Listview scrolled");
+      console.log(event.target);
+
+      let middle = 100 - Math.round((event.target.scrollTop) / (event.target.scrollHeight - event.target.offsetHeight) * 100);
+      this.scroller = "" + middle;
+
+    }
+    console.log("Listview scrolling");
+    this.automaticChange = false;
 
 
- // element.value = ""+ middle;
-}
+    // element.value = ""+ middle;
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
     this.menuService.getMenuItems().then((data) => {
@@ -60,24 +62,19 @@ listScrolled(event){
     console.log(item);
     if (item.hasOwnProperty("dbname")) {
       item.newLevel = this.level + 1;
-        this.navCtrl.push(MenuPage, item);
+      this.navCtrl.push(MenuPage, item);
     } else if (item.hasOwnProperty("contentDB")) {
       this.navCtrl.push(ContentPage, item);
     }
   }
 
   showVal() {
-    console.log("Scroller: " + this.scroller);
-    let selectingId = Math.round((this.items.length - 1) * (100 - Number(this.scroller)) / 100);
-    let element = document.getElementById("id-" +this.level + '-' + selectingId);
-    console.log(element);
-    console.log("id-" + selectingId);
-    if (element) {
-      console.log("Scrolling");
-      this.automaticChange = true;
-      element.scrollIntoView();
+    let scrollElement: HTMLDivElement = <HTMLDivElement>(document.getElementById('listScroll' + this.level).childNodes[0]);
 
-    }
-
+    let scrollValue = Number(this.scroller);
+    this.automaticChange = true;
+    scrollElement.scrollTop = (100 - scrollValue)/100 * (scrollElement.scrollHeight - scrollElement.offsetHeight);
+    console.log(scrollElement);
+    console.log(scrollElement.scrollTop);
   }
 }
